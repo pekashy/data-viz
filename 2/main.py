1038fbb28b04d9358ca7b7f4eb24c46efabe04b7
Egor Zyazev <zyazev.ea@phystech.edu>
2021-04-19 02:34:41 +0300

    Fix insertion. Add toggling

--- 2/main.py
+++ 2/main.py
@@ -4,6 +4,7 @@ from typing import Dict, List, Set, Tuple
 import networkx
 import matplotlib.pyplot as plt
 from queue import Queue
+from statistics import median
 
 DEFAULT_ENUMVAL: int = 1e12
 
@@ -14,7 +15,7 @@ class Node:
         self.val: str = val
         self.level: int = -1
         self.x: int = -1
-        self.enumVal = DEFAULT_ENUMVAL
+        self.enumVal : float = DEFAULT_ENUMVAL
 
     def addNode(self, child):
         if child:
@@ -23,6 +24,33 @@ class Node:
 
     def getNeighbors(self):
         return self.__neighbors
+
+    def toggle(self, visited : set, takenKoords : set):
+        if self in visited:
+            return
+        visited.add(self)
+        neighVals : List[int] = list()
+        for node in self.__neighbors:
+            neighVals.append(node.enumVal)
+        
+        if not neighVals:
+            return
+
+        takenKoords.add((self.level, self.enumVal))
+        bestKoord : int = int(median(neighVals))
+        i : int = 0
+        while (self.level, bestKoord + i) in takenKoords and (self.level, bestKoord - i) in takenKoords:
+            i+=1
+        if (self.level, bestKoord + i) not in takenKoords:
+            self.enumVal = bestKoord + i
+            takenKoords.add((self.level, self.enumVal))
+        elif (self.level, bestKoord - i) not in takenKoords:
+            self.enumVal = bestKoord - i
+            takenKoords.add((self.level, self.enumVal))
+
+        for node in self.__neighbors:
+            node.toggle(visited, takenKoords)
+
 
     def __ne__(self, other):
         return self.val != other.val
@@ -72,6 +100,11 @@ class Graph:
         for rawNode in rawNodes:
             self.__insertNode(None, rawNode)
         self.__startEnumerate()
+        toggled : Set[Node] = set()
+        takenKoords : set = set()
+        for node in self.nodes:
+            node.toggle(toggled, takenKoords)
+
 
     def __startEnumerate(self):
         first : Node = next(iter(self.nodes))
@@ -81,6 +114,7 @@ class Graph:
             self.__enumerate(currEnumVal)
         self.__levelEnumerate()
         pass
+
 
     def __levelEnumerate(self):
         q : Queue = Queue()
@@ -115,12 +149,18 @@ class Graph:
         newNode: Node = Node(rawNewNode.id)
         if newNode in self.nodes:
             return
-        self.nodes.add(newNode)
         if parent:
             parent.addNode(newNode)
+            self.nodes.add(newNode)
+            self.nodes.add(parent)
+        
+        if not rawNewNode.children():
+            self.nodes.add(newNode)
 
         for child in rawNewNode.children():
             self.__insertNode(newNode, child)
+    
+    
 
 added : Set[Node] = set()
 
@@ -128,10 +168,10 @@ def addNode(gr: networkx.DiGraph, node: Node):
     if node in added:
         return
     added.add(node)
-    nodeName: str = str(node.level) + " (" + str(node.enumVal) + ")"
+    nodeName: str = node.getVal()
     gr.add_node(nodeName, loc=(node.enumVal, node.level))
     for child in node.getNeighbors():
-        childName: str = str(child.level) + " (" + str(child.enumVal) + ")"
+        childName: str = child.getVal()
         gr.add_edge(nodeName, childName)
         addNode(gr, child)
 
